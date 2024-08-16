0f610ac8a51110f5fa297c98517924745ea321b6
LHBL2003 <Mister.Hempel@gmail.com>
2024-08-16 17:10:01 +0200

    The current status makes it possible to debug the inventory plugin.

--- netbox_qrcode/template_content.py
+++ netbox_qrcode/template_content.py
@@ -149,5 +149,15 @@ class PowerPanelQRCode(QRCode):
     def right_page(self):
         return self.Create_PluginContent()
 
+##################################
+# Other plugins support
+
+# Class for creating a QR code for the Plugin: Netbox-Inventory (https://github.com/ArnesSI/netbox-inventory)
+class Plugin_Netbox_Inventory(QRCode):
+    model = 'netbox_inventory.asset' # Info for Netbox in which model the plugin should be integrated.
+
+    def right_page(self):
+        return self.Create_PluginContent()
+
 # Connects Netbox Core with the plug-in classes
-template_extensions = [DeviceQRCode, RackQRCode, CableQRCode, LocationQRCode, PowerFeedQRCode, PowerPanelQRCode]
+template_extensions = [DeviceQRCode, RackQRCode, CableQRCode, LocationQRCode, PowerFeedQRCode, PowerPanelQRCode, Plugin_Netbox_Inventory]
